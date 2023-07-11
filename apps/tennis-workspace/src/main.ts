27dad3870609b35377020cc5896bb7acb6560e4f
theranjali-nilaweera <theranjali@goodhuman.me>
2023-07-11 20:24:23 +1000

    feature: add swagger

--- apps/tennis-workspace/src/main.ts
+++ apps/tennis-workspace/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+  .setTitle('Tennis Tournament Score API')
+  .setDescription('The Tennis Tournament Score API')
+  .setVersion('1.0')
+  .addTag('tennis-tournament-scores')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
