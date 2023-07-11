c9d268bba498782d8bb513a5cf173d71213b0284
theranjali-nilaweera <theranjali@goodhuman.me>
2023-07-11 23:26:44 +1000

    doc: readme changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,16 +8,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
-import path from 'path';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-
-  // app.useStaticAssets(path.join(__dirname,'..','assets'));
 
   const config = new DocumentBuilder()
     .setTitle('Tennis Tournament Score API')
